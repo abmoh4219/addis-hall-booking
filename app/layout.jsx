1bd6f26c3c552b52455d3ba1361ca838e741a433
abmoh4219 <abdelahmohammed0919@gmail.com>
2025-02-24 22:32:08 +0300

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import "@/assets/styles/globals.css";
+
+const inter=Inter({subsets: ['latin']});
+
+export const metadata = {
+  title: "Addis Hall Booking | Book a Room",
+  description: "Book conference halls in Addis Ababa with ease.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header/>
+        <main className="mx-auto max-w-7x1 px-4 py-6 sm:px-4 lg:px-7">
+          {children}
+        </main>
+
+        <Footer/>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import rooms from '@/data/rooms.json';
+
+export default function Home() {
+  return (
+    <>
+      {rooms.length > 0 ? (
+        rooms.map((room) => 
+          <h3 key={room.id}>{room.name}</h3>
+        )
+      ) : (
+        <p>No rooms available at the moment</p>
+      )}
+    </>
+  );
+}
